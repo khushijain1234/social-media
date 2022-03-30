41ff537d16a6d535983532498776d11a3c3cf748
Khushi <khushijain4332@gmail.com>
2022-03-30 14:30:58 +0530

    added server link

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API = axios.create({ baseURL: 'http://localhost:5000/'})
+const API = axios.create({ baseURL: 'https://warm-lake-26915.herokuapp.com/'})
 API.interceptors.request.use((req)=>{
     if(localStorage.getItem('profile')){
         req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`
